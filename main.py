831da6382a1d7b08a47e854dea0166e680bbfb72
JVMoreiraD <jvitormoreirad@hotmail.com>
2026-08-31 00:56:36 -0300

    feat: iniciando um projeto de reconhecimento transcrição de texto.

---
+++ main.py
@@ -0,0 +1,8 @@
+from listen_and_transcribe import listen_and_transcribe
+
+
+def main():
+    listen_and_transcribe()
+
+
+main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Objetivo
+
+Criar um script basico que capture voz e transforme para texto usando python.
