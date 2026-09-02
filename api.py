6f0330585d674913fe5d3b557ccee5be43479234
JVMoreiraD <jvitormoreirad@hotmail.com>
2026-09-02 00:50:29 -0300

    feat: meudança para modo stream

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 url = "http://127.0.0.1:1234/v1/chat/completions"
@@ -24,3 +26,33 @@ def request(question: str):
     response.raise_for_status()
 
     return response.json()["choices"][0]["message"]["content"]
+
+def request_stream(question: str):
+    payload = {
+        "model": "local-model",
+        "messages": [
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": question}
+        ],
+        "temperature": 0.7,
+        "stream": True,
+    }
+
+    with requests.post(url, json=payload, stream=True) as response:
+        response.raise_for_status()
+
+        for line in response.iter_lines():
+            if not line:
+                continue
+
+            if line.startswith(b"data: "):
+                data = line[len(b"data: "):]
+
+                if data == b"[DONE]":
+                    break
+
+                chunk = json.loads(data)
+                content = chunk["choices"][0]["delta"].get("content")
+
+                if content:
+                    yield content
